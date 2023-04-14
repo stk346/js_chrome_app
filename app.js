66d0fd70d1a8ddce58d1cc58b2bc577a34695746
stk346 <stk346@gamil.com>
2023-04-14 14:10:33 +0900

    eventListner

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleclick() {
+    title.style.color="blue";
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleclick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
