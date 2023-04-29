5170bdeeae58c7a623cb6da4f19d7418ca891fa8
stk346 <stk346@gamil.com>
2023-04-29 16:11:26 +0900

    todo-list 삭제하기 기능 구현

---
+++ js/todo.js
@@ -0,0 +1,31 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+
+    const button = document.createElement("button");
+    button.innerText = "♢";
+    button.addEventListener("click", deleteTodo);
+
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    paintTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit);
