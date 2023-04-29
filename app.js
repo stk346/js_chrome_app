2faeacd5e7a0408490b03559aa303e93ccc7d9a2
stk346 <stk346@gamil.com>
2023-04-29 13:47:46 +0900

    로그인 유무에 따라서 폼 숨기고 greeting 메시지 보여주기 기능 구현

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleclick() {
-    h1.classList.toggle("clicked");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreeting();
 }
 
-h1.addEventListener("click", handleTitleclick);
+function paintGreeting() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting();
+}
